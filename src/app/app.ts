97d9892f078421833d8cfc6413c038d9eabd784e
Andrii Polezhaiev <workpolezhaievcv@gmail.com>
2026-02-19 00:36:36 +0200

    add simple functionality for cars

--- src/app/app.ts
+++ src/app/app.ts
@@ -33,5 +33,32 @@ export class App implements OnInit {
       }
     });
   }
+
+  deleteCarById(id: number): void {
+    if (confirm('Вы уверены, что хотите удалить эту машину?')) {
+      this.carService.deleteCarById(id).subscribe({
+        next: () => {
+          this.getAllCars(); 
+        },
+        error: (err) => console.error('Ошибка при удалении', err)
+      });
+    }
+  }
+
+  updateCar(car: Car): void {
+  const newName = prompt('Введите новое название для машины:', car.name);
+
+  if (newName && newName !== car.name) {
+    const updatedCar: Car = { ...car, name: newName };
+
+    this.carService.updateCar(car.id, updatedCar).subscribe({
+      next: (response) => {
+        console.log('Машина обновлена:', response);
+        this.getAllCars();
+      },
+      error: (err) => console.error('Ошибка при обновлении:', err)
+    });
+  }
+}
 }
 
